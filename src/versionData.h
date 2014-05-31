30776f21fab5f8a9147602647e975913d585d36a
Jacob Causon <jake.causon@gmail.com>
2014-05-31 11:58:58 +0100

    fix/Updated version number

--- src/versionData.h
+++ src/versionData.h
@@ -3,7 +3,7 @@
 // VERSION is a human readable version of the version number
 // and V_* are the parts of the version which can be used by
 // a computer
-#define VERSION "v0.9 (beta)"
+#define VERSION "v0.9.1 (beta)"
 #define V_MAJOR 0
 #define V_MINOR 9
-#define V_PATCH 0
+#define V_PATCH 1
